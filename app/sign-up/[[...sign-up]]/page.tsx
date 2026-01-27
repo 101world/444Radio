5d3033fedcb98e979cc6fe5ba9a02116121cf880
101world <welcometo101world@gmail.com>
2026-01-27 14:41:31 +0530

    Remove deprecated afterSignUpUrl/afterSignInUrl props

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -178,8 +178,6 @@ function SignUpContent() {
         forceRedirectUrl="https://444radio.co.in"
         fallbackRedirectUrl="https://444radio.co.in"
         signInUrl="/sign-in"
-        afterSignUpUrl="https://444radio.co.in"
-        afterSignInUrl="https://444radio.co.in"
         appearance={{
           elements: {
             formButtonPrimary: 'bg-gradient-to-r from-cyan-400 via-cyan-500 to-teal-400 text-black font-bold hover:shadow-cyan-500/50 hover:scale-105 transition-all duration-200',
