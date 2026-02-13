4a7d87a0344cf418f7dd7ea34971a45a21ac62e5
101world <welcometo101world@gmail.com>
2026-02-13 20:50:06 +0530

    db: add generation_extract to credit_transactions CHECK constraint

---
+++ db/migrations/1020_add_generation_extract_type.sql
@@ -0,0 +1,39 @@
+-- Add 'generation_extract' to credit_transactions type CHECK constraint
+-- Required for the Extract Audio feature (video-to-audio + stem extraction)
+
+DO $$
+BEGIN
+  -- Drop existing constraint
+  IF EXISTS (
+    SELECT 1 FROM information_schema.table_constraints
+    WHERE table_name = 'credit_transactions'
+      AND constraint_type = 'CHECK'
+      AND constraint_name = 'credit_transactions_type_check'
+  ) THEN
+    ALTER TABLE credit_transactions DROP CONSTRAINT credit_transactions_type_check;
+  END IF;
+
+  -- Re-create with ALL types including generation_extract
+  ALTER TABLE credit_transactions ADD CONSTRAINT credit_transactions_type_check
+    CHECK (type IN (
+      'generation_music',
+      'generation_effects',
+      'generation_loops',
+      'generation_image',
+      'generation_video_to_audio',
+      'generation_cover_art',
+      'generation_stem_split',
+      'generation_audio_boost',
+      'generation_extract',
+      'earn_list',
+      'earn_purchase',
+      'earn_sale',
+      'earn_admin',
+      'credit_award',
+      'credit_refund',
+      'subscription_bonus',
+      'code_claim',
+      'release',
+      'other'
+    ));
+END $$;
